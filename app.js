fd4fb6b084df014e3f14debdb78354ec2ee68cb5
Jam Risser <jam@jamrizzi.com>
2017-01-28 21:48:47 -0600

    Go to the source

--- app.js
+++ app.js
@@ -3,133 +3,178 @@ var _ = require('lodash');
 var fetch = require('node-fetch');
 var fs = require('fs');
 var changeCase = require('change-case');
+var commander = require('commander');
 
-var options = {
-    usernameLength: 3,
-    verbose: false,
-    token: ''
-};
-
-if (process.env.NAMECHK_TOKEN) options.token = process.env.NAMECHK_TOKEN;
-var args = getArgs();
-if (args.site) {
-    options.site = args.site;
-} else {
-    console.log('Must specify site');
-    process.exit();
-}
-if (args.verbose) options.verbose = args.verbose;
-
-var usernames = getCombinations(options.usernameLength, 'abcdefghijklmnopqrstuvwxyz');
-var groups = getUsernameGroups(usernames);
-console.log('Username Combinations: ' + usernames.length);
-_.each(options, function(option, key) {
-    console.log(changeCase.titleCase(key) + ': ' + option);
-});
-console.log('');
-getAvailableUsernames(groups, options.site);
-
-function getArgs() {
-    var args = {
-        verbose: false,
-        site: false
-    };
-    _.each(process.argv, function(arg) {
-        if (arg.substr(arg.length - 4) !== 'node' && arg.substr(arg.length - 3) !== '.js') {
-            if (arg === '--verbose' || arg === '-v') {
-                args.verbose = true;
-            } else {
-                args.site = arg;
-            }
-        }
-    });
-    return args;
+var defaults = {
+	version: '0.0.2',
+	chars: 'abcdefghijklmnopqrstuvwxyz',
+	min: 3,
+	max: 4,
+	regex: '',
+	results: 25000,
+	site: 'twitter',
+	verbose: false
 }
 
-function getCombinations(count, alphabet, word) {
-    var combinations = [];
-    if (count > 0) {
-        _.each(alphabet, function(char) {
-            var fullWord = getCombinations(count - 1, alphabet, (word ? word : '') + char);
-            if (fullWord) {
-                combinations.push(fullWord);
-            }
-        });
-        if (word) {
-            if (combinations.length > 0) return combinations;
-        } else {
-            return _.flattenDeep(combinations);
-        }
-    } else {
-        return word;
-    }
+function main() {
+	commander.version(defaults.version)
+		.description('Checks for available usernames')
+		.option('-v --verbose', 'Verbose mode')
+		.option('-c --chars [chars]', 'Characters used to generate combinations')
+		.option('--max [max]', 'Maximum length')
+		.option('--min [min]', 'Minimum length')
+		.option('-r --regex [regex]', 'Regular expression used to filter combinations')
+		.option('--results [results]', 'Number of results to check')
+		.parse(process.argv);
+	var options = getOptions(defaults, commander);
+	var combinations = getCombinations(
+		options.chars,
+		options.min,
+		options.max,
+		new RegExp(options.regex),
+		options.results);
+	getAvailableUsername(combinations, options.site, options.verbose).then(function(usernames) {
+		if (options.verbose) {
+			console.log('');
+			if (usernames.length > 0) console.log(usernames);
+		}
+		if (usernames.length <= 0) console.log('No available usernames found');
+	}).catch(function(err) {
+		if (options.verbose) console.error(err.message);
+	});
 }
 
-function getUsernameGroups(usernames) {
-    var groups = [];
-    var group = [];
-    var count = 0;
-    _.each(usernames, function(username) {
-        group.push(username);
-        if (group.length > 5 || count >= usernames.length) {
-            groups.push(group);
-            group = [];
-        }
-    });
-    return groups;
-};
+function getOptions(defaults, commander) {
+	var options = {
+		chars: commander.chars ? commander.chars : defaults.chars,
+		min: commander.min ? Number(commander.min) : defaults.min,
+		max: commander.max ? Number(commander.max) : defaults.max,
+		regex: commander.regex ? commander.regex : defaults.regex,
+		results: commander.results ? Number(commander.results) : defaults.results,
+		verbose: commander.verbose ? commander.verbose : defaults.verbose,
+		site: commander.args[0] ? commander.args[0] : defaults.site
+	};
+	_.each(options, function(option, key) {
+		console.log(changeCase.titleCase(key) + ': ' + option);
+	});
+	console.log('-----------------------\n');
+	return options;
+}
 
-function getAvailableUsernames(groups, site) {
-    return new Promise(function(resolve, reject) {
-        fs.open(site + '.log', 'a', undefined, function(err, fd) {
-            if(err) throw err;
-            var count = 0;
-            var promises = _.map(groups, function(group) {
-                return fetch('https://api.namechk.com/services/bulk_check.json', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        AUTHORIZATION: 'Bearer ' + options.token,
-                        Accept: 'application/vnd.api.v1+json'
-                    },
-                    body: JSON.stringify({
-                        site: site,
-                        usernames: group
-                    })
-                }).then(function(res) {
-                    return res.json();
-                }).then(function(body) {
-                    var available = [];
-                    var promises = _.map(body, function(result) {
-                        return new Promise(function(resolve, reject) {
-                            if (options.verbose) console.log(count++);
-                            if (result.available) {
-                                console.log(result.username);
-                                available.push(result.username);
-                                fs.write(fd, result.username + '\n', undefined, undefined, function(err) {
-                                    if (err) reject(err);
-                                    resolve(result.username);
-                                });
-                            } else {
-                                resolve(false);
-                            }
-                        });
-                    });
-                    return Promise.all(promises).then(function(messages) {
-                        if (available.length > 0) {
-                            return available;
-                        } else {
-                            return false;
-                        }
-                    });
-                });
-            });
-            Promise.all(promises).then(function(groups) {
-                fs.close(fd, function(err) {
-                    if (err) reject(err);
-                    resolve(groups);
-                });
-            });
-        });
-    });
-};
+function getAvailableUsername(combinations, site, verbose) {
+	prettySite = changeCase.titleCase(site);
+	return new Promise(function(resolve, reject) {
+		fs.open(site + '.log', 'a', undefined, function(err, fd) {
+			if (err) reject(err);
+			resolve(fd);
+		});
+	}).then(function(fd) {
+		var promises = _.map(combinations, function(combination) {
+			if (verbose) console.log('Trying ' + prettySite + ' Combination: ' + combination);
+			return checkSite(combination, site).then(function(username) {
+				return new Promise(function(resolve, reject) {
+					if (username) {
+						console.log('Available ' + prettySite + ' Username: ' + username);
+						fs.write(fd, username + '\n', undefined, undefined, function(err) {
+							if (err) reject(err);
+							resolve(username);
+						});
+					} else {
+						resolve(undefined);
+					}
+				});
+			});
+		});
+		return Promise.all(promises).then(function(usernames) {
+			usernames = _.filter(usernames, function(username) {
+				return !!username;
+			});
+			return {
+				usernames: usernames,
+				fd: fd
+			};
+		});
+	}).then(function(data) {
+		var usernames = data.usernames;
+		var fd = data.fd;
+		return new Promise(function(resolve, reject) {
+			fs.close(fd, function(err) {
+				if (err) reject(err);
+				resolve(usernames);
+			});
+		});
+	}).then(function(usernames) {
+		return usernames;
+	});
+}
+
+function checkSite(combination, site) {
+	return new Promise(function(resolve, reject) {
+		switch(site) {
+		case 'twitter':
+			checkTwitter(combination).then(function(username) {
+				resolve(username);
+			}).catch(function(err) {
+				reject(err);
+			});
+			break;
+		case 'wordpress':
+			checkWordPress(combination).then(function(username) {
+				resolve(username);
+			}).catch(function(err) {
+				reject(err);
+			});
+			break;
+		default:
+			resolve(undefined);
+			break;
+		}
+	});
+}
+
+// This is the brains of the operation (DO NOT TOUCH)
+function getCombinations(chars, min, max, regex, results) {
+	var combinations = [];
+	for (var i = 0; i <= (max - min); i++) {
+		getCombinationsOfLength(min + i, chars, regex);
+	}
+	function getCombinationsOfLength(count, chars, regex) {
+		_getCombinations(count, chars);
+		function _getCombinations(count, chars, word) {
+			if (count > 0) {
+				_.each(chars, function(char) {
+					var fullWord = _getCombinations(count - 1, chars, (word ? word : '') + char);
+					if (fullWord) {
+						if (combinations && combinations.length >= results) return false;
+						combinations.push(fullWord);
+						return null;
+					}
+					return null;
+				});
+			} else {
+				if (regex.test(word)) return word;
+			}
+			return false;
+		}
+	}
+	return combinations;
+}
+
+function checkTwitter(combination) {
+	return fetch('https://twitter.com/' + combination)
+		.then(function(response) {
+			if (response.status == 404) return combination;
+			return undefined;
+		});
+}
+
+function checkWordPress(combination) {
+	return fetch('https://' + combination + '.wordpress.com', {
+		redirect: 'manual'
+	}).then(function(response) {
+		if (response.status === 302) return combination;
+		return undefined;
+	});
+}
+
+main();
